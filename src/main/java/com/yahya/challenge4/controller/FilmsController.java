9718aff93694a2e8d8e3d8714eeda02da3cb805e
Yahya Wijaya <yahyawijaya082@gmail.com>
2022-04-22 21:00:40 +0700

    kelima benerin invoice controller

--- src/main/java/com/yahya/challenge4/controller/FilmsController.java
+++ src/main/java/com/yahya/challenge4/controller/FilmsController.java
@@ -28,43 +28,78 @@ public class FilmsController {
                     content = @Content) })
     @Operation(summary = "menambahkan film ke tabel film pada database")
     @PostMapping("/add-film")
-    public String addFilms(String nameFilm, Boolean Tayang){
+    public String addFilms(@Schema(example = "{" +
+            "\"nameFilm\":\"Programmer naik haji\"," +
+            "\"Tayang\":\"1\"" +
+            "}") String nameFilm, Boolean Tayang){
         filmService.addFilms(nameFilm,Tayang);
         return"film berhasil ditambahkan";
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Berhasil Menampilkan FIlm",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Films.class)) })})
     @Operation(summary = "menampilkan film sesuai dengan code film yang dimasukan")
     @GetMapping(value="/get-film/{codeFilm}")
-    public Films getFilms(@PathVariable("codeFilm") Long codeFilm) {
+    public Films getFilms(@Schema(example = "{" +
+            "\"codeFilm\":\"1\"," +
+            "}")@PathVariable("codeFilm") Long codeFilm) {
         Films film = filmService.getFilm(codeFilm).get();
         System.out.println("Film Name : " + film.getFilmName() + "\nTayang : " + film.getTayang());
         return film;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Berhasil meng-Update FIlm",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Films.class)) })})
     @Operation(summary = "mengubah isi tabel film yang sudah ada pada database")
     @PutMapping(value="/update-film")
-    public String updateFilm(Long filmCode, String filmName, Boolean Tayang){
+    public String updateFilm(@Schema(example = "{" +
+            "\"filmCode\":\"1\"," +
+            "\"filmName\":\"Programmer naik haji\"," +
+            "\"Tayang\":\"1\"" +
+            "}")Long filmCode, String filmName, Boolean Tayang){
         filmService.updateFilm(filmCode,filmName,Tayang);
         return "update berhasil";
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Berhasil Menghapus FIlm",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Films.class)) })})
     @Operation(summary = "menghapus isi tabel film sesuai dengan nama film")
     @DeleteMapping(value="/delete-film/{filmName}")
-    public String deleteFilm(@PathVariable("filmName")String filmName){
+    public String deleteFilm(@Schema(example = "{" +
+            "\"filmName\":\"Programmer naik haji\"," +
+            "}")@PathVariable("filmName")String filmName){
         filmService.deleteFilm(filmName);
         return "berhasil menghapus";
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Berhasil Menghapus FIlm",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Films.class)) })})
     @Operation(summary = "menghapus isi tabel film sesuai dengan code film")
     @DeleteMapping(value = "/delete-film-by-id/{filmCode}")
-    public  String deleteFilmById(@PathVariable("filmCode")Long filmCode){
+    public  String deleteFilmById(@Schema(example = "{" +
+            "\"filmCode\":\"1\"," +
+            "}")@PathVariable("filmCode")Long filmCode){
         filmService.deleteFilmById(filmCode);
         return  "berhasil dihapus";
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Berhasil Menampilkan FIlm",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Films.class)) })})
     @Operation(summary = "menampilkan isi tabel film yang sedang tayang")
     @GetMapping(value = "/film-sedang-tayang")
-    public List<Films> filmTayang(Boolean Tayang){
+    public List<Films> filmTayang(@Schema(example = "{" +
+            "\"tayang\":\"1\"," +
+            "}")Boolean Tayang){
         List<Films> films = filmService.getFilmTayang(Tayang);
         films.forEach(film ->{
             System.out.println(film.getFilmName());
@@ -72,6 +107,10 @@ public class FilmsController {
         return films;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Berhasil Menampilkan FIlm",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Films.class)) })})
     @Operation(summary = "menampilkan semua isi tabel pada tabel film")
     @RequestMapping(value ="/film",method = RequestMethod.GET)
     public List<Films> allFilms(){
--- src/main/java/com/yahya/challenge4/controller/InvoiceController.java
+++ src/main/java/com/yahya/challenge4/controller/InvoiceController.java
@@ -1,27 +1,18 @@
 package com.yahya.challenge4.controller;
 
-import com.yahya.challenge4.model.Schedules;
-import com.yahya.challenge4.model.Seats;
-import com.yahya.challenge4.model.Users;
 import com.yahya.challenge4.service.InvoiceService;
 import com.yahya.challenge4.service.SchedulesService;
 import com.yahya.challenge4.service.SeatsService;
 import com.yahya.challenge4.service.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
-import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/invoice/api")
@@ -38,35 +29,42 @@ public class InvoiceController {
 
     @Operation(summary = "menampilkan tiket dengan format pdf")
     @GetMapping("/tiket-bioskop")
-    public void generateInvoice(HttpServletResponse response) throws IOException, JRException {
-        JasperReport sourceFileName = JasperCompileManager
-                .compileReport(ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX
-                        + "tiketBioskop.jrxml").getAbsolutePath());
-
-        // creating our list of beans
-//        List<Users> dataList = new ArrayList<>();
-        List<Map<String, String>> dataList = new ArrayList<>();
-        Map<String, String> data = new HashMap<>();
-        Users user = usersService.getUsersByUsername("yahya");
-        data.put("username", user.getUsername());
-        Seats seat = seatsService.getNoKursi(1,"A");
-        data.put("noKursi", String.valueOf(seat.getId().getNoKursi()));
-        data.put("studioName", String.valueOf(seat.getId().getStudioName()));
-        Schedules schedule = schedulesService.getSchedulesById(1L);
-        data.put("filmName", String.valueOf(schedule.getFilmCode().getFilmName()));
-        data.put("tglTayang",schedule.getTglTayang());
-        data.put("jamMulai",schedule.getJamMulai());
-        data.put("jamSelesai",schedule.getJamSelesai());
-        dataList.add(data);
-
-        // creating datasource from bean list
-        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
-        Map<String, Object> parameters = new HashMap();
-        parameters.put("createdBy", "Yahya");
-        JasperPrint jasperPrint = JasperFillManager.fillReport(sourceFileName, parameters, beanColDataSource);
-        response.setContentType("application/pdf");
-        response.addHeader("Content-Disposition", "inline; filename=tiketBioskop.pdf;");
-
-        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    public void generateInvoice1(HttpServletResponse response) throws JRException, IOException {
+        invoiceService.generateInvoice(response);
     }
+
+
+//    @Operation(summary = "menampilkan tiket dengan format pdf")
+//    @GetMapping("/tiket-bioskop")
+//    public void generateInvoice(HttpServletResponse response) throws IOException, JRException {
+//        JasperReport sourceFileName = JasperCompileManager
+//                .compileReport(ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX
+//                        + "tiketBioskop.jrxml").getAbsolutePath());
+//
+//        // creating our list of beans
+////        List<Users> dataList = new ArrayList<>();
+//        List<Map<String, String>> dataList = new ArrayList<>();
+//        Map<String, String> data = new HashMap<>();
+//        Users user = usersService.getUsersByUsername("yahya");
+//        data.put("username", user.getUsername());
+//        Seats seat = seatsService.getNoKursi(1,"A");
+//        data.put("noKursi", String.valueOf(seat.getId().getNoKursi()));
+//        data.put("studioName", String.valueOf(seat.getId().getStudioName()));
+//        Schedules schedule = schedulesService.getSchedulesById(1L);
+//        data.put("filmName", String.valueOf(schedule.getFilmCode().getFilmName()));
+//        data.put("tglTayang",schedule.getTglTayang());
+//        data.put("jamMulai",schedule.getJamMulai());
+//        data.put("jamSelesai",schedule.getJamSelesai());
+//        dataList.add(data);
+//
+//        // creating datasource from bean list
+//        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
+//        Map<String, Object> parameters = new HashMap();
+//        parameters.put("createdBy", "Yahya");
+//        JasperPrint jasperPrint = JasperFillManager.fillReport(sourceFileName, parameters, beanColDataSource);
+//        response.setContentType("application/pdf");
+//        response.addHeader("Content-Disposition", "inline; filename=tiketBioskop.pdf;");
+//
+//        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+//    }
 }
